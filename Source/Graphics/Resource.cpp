a4fd7d84baa5f5e28ccf4d75f5303fcc89435626
gokalpcevik <cevikgokalp8@gmail.com>
2022-09-05 22:05:06 +0300

    Resource class for structure
    Added a resource class for less boilerplate code in renderer, + added some functionality for transitions and uploading data to the resource heap

---
+++ Source/Graphics/Resource.cpp
@@ -0,0 +1,83 @@
+#include "Resource.h"
+
+namespace acc3d::Graphics
+{
+	using Microsoft::WRL::ComPtr;
+
+	Resource::Resource(ID3D12Device* pDevice, D3D12_HEAP_PROPERTIES const* pHeapProperties, D3D12_HEAP_FLAGS HeapFlags,
+		D3D12_RESOURCE_DESC const* pDesc, D3D12_RESOURCE_STATES InitialResourceState,
+		D3D12_CLEAR_VALUE const* pOptimizedClearValue)
+	: m_Device(pDevice)
+	{
+		THROW_IFF(pDevice->CreateCommittedResource(pHeapProperties, HeapFlags, pDesc, InitialResourceState, pOptimizedClearValue,
+			IID_PPV_ARGS(&m_Resource)));
+	}
+
+	void Resource::UpdateBufferResource(ID3D12GraphicsCommandList2* pCmdList, size_t NumElems, size_t ElemSize,
+		void const* pBufferData, D3D12_RESOURCE_FLAGS flags)
+	{
+		ComPtr<ID3D12Resource> intermediateResource;
+		Resource::UpdateBufferResource(m_Device, pCmdList, &m_Resource, &intermediateResource, NumElems, ElemSize,
+		                               pBufferData, flags);
+	}
+
+	void Resource::TransitionAndBarrier(ID3D12GraphicsCommandList2* pGfxCmdList, D3D12_RESOURCE_STATES stateBefore,
+		D3D12_RESOURCE_STATES stateAfter) const
+	{
+		Resource::TransitionAndBarrier(m_Resource.Get(), pGfxCmdList, stateBefore, stateAfter);
+	}
+
+	void Resource::UpdateBufferResource(ID3D12Device* pDevice, ID3D12GraphicsCommandList2* pCmdList, ID3D12Resource** pDestinationResource,
+	                                    ID3D12Resource** pIntermediateResource, size_t NumElems, size_t ElemSize, void const* bufferData,
+	                                    D3D12_RESOURCE_FLAGS flags)
+	{
+		size_t bufferSizeInBytes = NumElems * ElemSize;
+
+		auto const destinationHeapProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
+		auto const destinationResourceDescription = CD3DX12_RESOURCE_DESC::Buffer(bufferSizeInBytes, flags);
+
+		THROW_IFF(pDevice->CreateCommittedResource(
+			&destinationHeapProperties,
+			D3D12_HEAP_FLAG_NONE,
+			&destinationResourceDescription,
+			D3D12_RESOURCE_STATE_COPY_DEST,
+			nullptr,
+			IID_PPV_ARGS(pDestinationResource)));
+
+		if (bufferData)
+		{
+			auto const intermediateHeapProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+			auto const intermediateResourceDescription = CD3DX12_RESOURCE_DESC::Buffer(bufferSizeInBytes);
+			THROW_IFF(pDevice->CreateCommittedResource(
+				&intermediateHeapProperties,
+				D3D12_HEAP_FLAG_NONE,
+				&intermediateResourceDescription,
+				D3D12_RESOURCE_STATE_GENERIC_READ,
+				nullptr,
+				IID_PPV_ARGS(pIntermediateResource)));
+			D3D12_SUBRESOURCE_DATA subresourceData = {};
+			subresourceData.pData = bufferData;
+			subresourceData.RowPitch = bufferSizeInBytes;
+			subresourceData.SlicePitch = subresourceData.RowPitch;
+
+			UpdateSubresources(pCmdList,
+				*pDestinationResource, *pIntermediateResource,
+				0, 0, 1, &subresourceData);
+		}
+	}
+
+	void Resource::UpdateBufferResource(ID3D12Device* pDevice, ID3D12GraphicsCommandList2* pCmdList,
+		Resource& DestinationResource,Resource& IntermediateResource, size_t NumElems, size_t ElemSize,
+		void const* bufferData, D3D12_RESOURCE_FLAGS flags)
+	{
+		Resource::UpdateBufferResource(pDevice, pCmdList, &DestinationResource.m_Resource, &IntermediateResource.m_Resource, NumElems, ElemSize,bufferData, flags);
+	}
+
+	void Resource::TransitionAndBarrier(ID3D12Resource* pResource, ID3D12GraphicsCommandList2* pGfxCmdList,
+	                                    D3D12_RESOURCE_STATES stateBefore, D3D12_RESOURCE_STATES stateAfter)
+	{
+		CD3DX12_RESOURCE_BARRIER transitionResourceBarrier = CD3DX12_RESOURCE_BARRIER::Transition(
+			pResource, stateBefore, stateAfter);
+		pGfxCmdList->ResourceBarrier(1UL, &transitionResourceBarrier);
+	}
+}
---
+++ Source/Graphics/Resource.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <d3d12.h>
+#include <d3dx12.h>
+#include <wrl.h>
+#include "ResultHandler.h"
+
+namespace acc3d::Graphics
+{
+	class Resource
+	{
+	public:
+		// Constructor for committed resources
+		Resource(ID3D12Device* pDevice, D3D12_HEAP_PROPERTIES const* pHeapProperties, D3D12_HEAP_FLAGS HeapFlags, 
+			D3D12_RESOURCE_DESC const* pDesc, D3D12_RESOURCE_STATES InitialResourceState, 
+			D3D12_CLEAR_VALUE const* pOptimizedClearValue);
+
+		void UpdateBufferResource(ID3D12GraphicsCommandList2* pCmdList, size_t NumElems, size_t ElemSize,
+		                          void const* pBufferData,
+		                          D3D12_RESOURCE_FLAGS flags = D3D12_RESOURCE_FLAG_NONE);
+
+		void TransitionAndBarrier(ID3D12GraphicsCommandList2* pGfxCmdList, D3D12_RESOURCE_STATES stateBefore,
+		                          D3D12_RESOURCE_STATES stateAfter) const;
+
+		static void UpdateBufferResource(ID3D12Device* pDevice, ID3D12GraphicsCommandList2* pCmdList,
+		                                 ID3D12Resource** pDestinationResource,
+		                                 ID3D12Resource** pIntermediateResource, size_t NumElems, size_t ElemSize, void const* bufferData,
+		                                 D3D12_RESOURCE_FLAGS flags = D3D12_RESOURCE_FLAG_NONE);
+
+		static void UpdateBufferResource(ID3D12Device* pDevice, ID3D12GraphicsCommandList2* pCmdList,
+		                                Resource& DestinationResource, Resource& IntermediateResource, size_t NumElems,
+		                                size_t ElemSize, void const* bufferData,
+		                                D3D12_RESOURCE_FLAGS flags = D3D12_RESOURCE_FLAG_NONE);
+
+		static void TransitionAndBarrier(ID3D12Resource* pResource, ID3D12GraphicsCommandList2* pGfxCmdList,
+		                       D3D12_RESOURCE_STATES stateBefore, D3D12_RESOURCE_STATES stateAfter);
+
+	private:
+		Microsoft::WRL::ComPtr<ID3D12Resource> m_Resource;
+		// Non-owning pointer to the D3D12Device 
+		ID3D12Device* m_Device = nullptr;
+	};
+}
