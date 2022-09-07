7f61d0a478c736f8c2eb2d840cf61dd83b0c1c77
gokalpcevik <cevikgokalp8@gmail.com>
2022-09-07 23:31:30 +0300

    Vertex Layout Deduction from ID3D12ShaderReflection

---
+++ Source/Graphics/ShaderReflection.cpp
@@ -0,0 +1,146 @@
+#include "ShaderReflection.h"
+
+namespace acc3d::Graphics
+{
+	using Microsoft::WRL::ComPtr;
+
+	DXGI_FORMAT ShaderDataFormatHelper::ShaderDataFormatToDXGIFormat(ShaderDataFormat format)
+	{
+		switch (format) {
+		default:						  return DXGI_FORMAT_UNKNOWN;
+		case ShaderDataFormat::Unknown:   return DXGI_FORMAT_UNKNOWN;
+		case ShaderDataFormat::Float32_1: return DXGI_FORMAT_R32_FLOAT;
+		case ShaderDataFormat::Float32_2: return DXGI_FORMAT_R32G32_FLOAT;
+		case ShaderDataFormat::Float32_3: return DXGI_FORMAT_R32G32B32_FLOAT;
+		case ShaderDataFormat::Float32_4: return DXGI_FORMAT_R32G32B32A32_FLOAT;
+		case ShaderDataFormat::UInt32_1:  return DXGI_FORMAT_R32_UINT;
+		case ShaderDataFormat::UInt32_2:  return DXGI_FORMAT_R32G32_UINT;
+		case ShaderDataFormat::UInt32_3:  return DXGI_FORMAT_R32G32B32_UINT;
+		case ShaderDataFormat::UInt32_4:  return DXGI_FORMAT_R32G32B32A32_UINT;
+		case ShaderDataFormat::SInt32_1:  return DXGI_FORMAT_R32_SINT;
+		case ShaderDataFormat::SInt32_2:  return DXGI_FORMAT_R32G32_SINT;
+		case ShaderDataFormat::SInt32_3:  return DXGI_FORMAT_R32G32B32_SINT;
+		case ShaderDataFormat::SInt32_4:  return DXGI_FORMAT_R32G32B32A32_SINT;
+		}
+	}
+
+	uint64_t ShaderDataFormatHelper::GetShaderDataFormatSizeInBytes(ShaderDataFormat format)
+	{
+        switch (format) {
+        default:						  return 0ULL;
+        case ShaderDataFormat::Unknown:   return 0ULL;
+        case ShaderDataFormat::Float32_1: return 4ULL * 1ULL;
+        case ShaderDataFormat::Float32_2: return 4ULL * 2ULL;
+        case ShaderDataFormat::Float32_3: return 4ULL * 3ULL;
+        case ShaderDataFormat::Float32_4: return 4ULL * 4ULL;
+        case ShaderDataFormat::UInt32_1:  return 4ULL * 1ULL;
+        case ShaderDataFormat::UInt32_2:  return 4ULL * 2ULL;
+        case ShaderDataFormat::UInt32_3:  return 4ULL * 3ULL;
+        case ShaderDataFormat::UInt32_4:  return 4ULL * 4ULL;
+        case ShaderDataFormat::SInt32_1:  return 4ULL * 1ULL;
+        case ShaderDataFormat::SInt32_2:  return 4ULL * 2ULL;
+        case ShaderDataFormat::SInt32_3:  return 4ULL * 3ULL;
+        case ShaderDataFormat::SInt32_4:  return 4ULL * 4ULL;
+        }
+	}
+
+	VertexLayout::VertexLayout(ShaderId id)
+	{
+		assert(ShaderLibrary::IsLoaded(id) 
+			&&  R"(The shader that is to be reflected for the vertex layout is 
+					not loaded into the shader library.)");
+
+		auto& entry = ShaderLibrary::GetCompilationEntry(id);
+		assert(entry.ShaderType == ShaderType::Vertex 
+			&& "Shader needs to be a vertex shader to be reflected for the vertex layout deduction.");
+
+		ComPtr<ID3D12ShaderReflection> pReflection;
+
+		THROW_IFF(D3DReflect(entry.Blob->GetBufferPointer(), entry.Blob->GetBufferSize(), IID_PPV_ARGS(&pReflection)));
+
+		D3D12_SHADER_DESC shaderDesc;
+		THROW_IFF(pReflection->GetDesc(&shaderDesc));
+
+		m_NumInputParameters = shaderDesc.InputParameters;
+
+        uint64_t alignedByteOffset = 0;
+
+		for(size_t i = 0; i < m_NumInputParameters; ++i)
+		{
+			D3D12_SIGNATURE_PARAMETER_DESC sigParamDesc;
+			THROW_IFF(pReflection->GetInputParameterDesc(i, &sigParamDesc));
+
+			VertexLayoutElement layoutElement{};
+
+            if (sigParamDesc.Mask == 1)
+            {
+                if (sigParamDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32)
+                {
+                    layoutElement.Format = ShaderDataFormat::UInt32_1;
+                }
+                else if (sigParamDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32)
+                {
+	                layoutElement.Format = ShaderDataFormat::SInt32_1;
+                }
+                else if (sigParamDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32)
+                {
+                    layoutElement.Format = ShaderDataFormat::Float32_1;
+                }
+            }
+            else if (sigParamDesc.Mask <= 3)
+            {
+                if (sigParamDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32)
+                {
+                    layoutElement.Format = ShaderDataFormat::UInt32_2;
+                }
+                else if (sigParamDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32)
+                {
+                    layoutElement.Format = ShaderDataFormat::SInt32_2;
+
+                }
+                else if (sigParamDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32)
+                {
+                    layoutElement.Format = ShaderDataFormat::Float32_2;
+                }
+            }
+            else if (sigParamDesc.Mask <= 7)
+            {
+                if (sigParamDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32)
+                {
+                    layoutElement.Format = ShaderDataFormat::UInt32_3;
+                }
+                else if (sigParamDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32)
+                {
+                    layoutElement.Format = ShaderDataFormat::SInt32_3;
+                }
+                else if (sigParamDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32)
+                {
+                    layoutElement.Format = ShaderDataFormat::Float32_3;
+                }
+            }
+            else if (sigParamDesc.Mask <= 15)
+            {
+                if (sigParamDesc.ComponentType == D3D_REGISTER_COMPONENT_UINT32)
+                {
+                    layoutElement.Format = ShaderDataFormat::UInt32_4;
+                }
+                else if (sigParamDesc.ComponentType == D3D_REGISTER_COMPONENT_SINT32)
+                {
+                    layoutElement.Format = ShaderDataFormat::UInt32_4;
+                }
+                else if (sigParamDesc.ComponentType == D3D_REGISTER_COMPONENT_FLOAT32)
+                {
+                    layoutElement.Format = ShaderDataFormat::UInt32_4;
+                }
+            }
+            layoutElement.AlignedByteOffset = alignedByteOffset;
+            layoutElement.Size = ShaderDataFormatHelper::GetShaderDataFormatSizeInBytes(layoutElement.Format);
+            layoutElement.SemanticName = sigParamDesc.SemanticName;
+            layoutElement.SemanticIndex = sigParamDesc.SemanticIndex;
+            alignedByteOffset += layoutElement.Size;
+            m_Stride += layoutElement.Size;
+            m_LayoutElements.push_back(layoutElement);
+		}
+
+	}
+}
---
+++ Source/Graphics/ShaderReflection.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <d3d12.h>
+#include <d3dx12.h>
+#include <wrl.h>
+#include <vector>
+#include "ShaderLibrary.h"
+#include "ResultHandler.h"
+#include "Type.h"
+
+namespace acc3d::Graphics
+{
+	enum class ShaderDataFormat : uint32_t
+	{
+		Unknown = 0,
+		UInt32_1 = 1,
+		UInt32_2 = 2,
+		UInt32_3 = 3,
+		UInt32_4 = 4,
+		SInt32_1 = 5,
+		SInt32_2 = 6,
+		SInt32_3 = 7,
+		SInt32_4 = 8,
+		Float32_1 = 9,
+		Float32_2 = 10,
+		Float32_3 = 11,
+		Float32_4 = 12,
+	};
+
+
+	struct VertexLayoutElement
+	{
+		ShaderDataFormat Format;
+		std::string SemanticName;
+		uint64_t SemanticIndex;
+		uint64_t Register;
+		uint64_t Size;
+		uint64_t AlignedByteOffset;
+
+		
+	};
+
+	class ShaderDataFormatHelper
+	{
+	public:
+		ShaderDataFormatHelper() = default;
+
+		static DXGI_FORMAT ShaderDataFormatToDXGIFormat(ShaderDataFormat format);
+		static uint64_t GetShaderDataFormatSizeInBytes(ShaderDataFormat format);
+	};
+
+
+	class VertexLayout
+	{
+	public:
+		explicit VertexLayout(ShaderId id);
+
+	private:
+		std::vector<VertexLayoutElement> m_LayoutElements;
+		uint64_t m_NumInputParameters = 0;
+		uint64_t m_Stride = 0;
+	};
+}
